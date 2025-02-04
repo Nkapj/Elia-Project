0b5380e843234c6491cc8f7ec92c7aedcfd44cd6
Hamoir Chloe <chloehamoir@gmail.com>
2025-02-04 10:22:12 +0100

    creation login page relier à la page home

---
+++ front-end/src/App.jsx
@@ -0,0 +1,20 @@
+// le rendu JS 
+// import des rotes 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./pages/Home";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/Login.jsx
@@ -0,0 +1,59 @@
+// formulaire de connexion 
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+// initaliser les espace vide 
+const [name , SetName] = useState("");
+const [password, setPassword] = useState("");
+const navigate = useNavigate();// Hook pour naviguer entre les pages 
+
+// gerer les entrée du formulaire et ce qu'on fait quand on valide 
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // voir si les données correspondent il faudra changer avec les données de la base de donnés 
+if (name === "Jiara Martins" && password === "123456") {
+        // Si c'est correct, on redirige vers la page d'accueil
+    navigate("/home");
+      } else {
+        alert("Nom ou mot de passe incorrect.");
+      }
+    };
+
+
+  return (
+    <div className="login-container">
+    <h1 className="title">Login</h1>
+    <span className="subtitle">sign in</span>
+
+    <form className="login-form" onSubmit={handleSubmit}>
+      <label htmlFor="name">NAME</label>
+      <input
+        id="name"
+        type="text"
+        placeholder="Entrer le nom"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        
+      />
+
+      <label htmlFor="password">PASSWORD</label>
+      <input
+        id="password"
+        type="password"
+        placeholder="Entrer le mot de passe"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button className="login-button" type="submit">
+        sign up
+      </button>
+    </form>
+  </div>
+);
+}
+
+export default Login
